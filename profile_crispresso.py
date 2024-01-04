d21d1b1d79461cf87a588096048fcf547ab8beaf
McKay <mbowcut@gmail.com>
2024-01-04 14:52:25 -0700

    report elapsed time in profile script

--- profile_crispresso.py
+++ profile_crispresso.py
@@ -1,8 +1,20 @@
 import subprocess
+import sys
+import time
+
+# TODO: collect baseline measures for commands of interest
+# We are more interested in larger jobs, so we can see any 
+# performance improvements.
+# - Cole suggested a batch run, there may be a batch run in tests
+# - We can run the make commands
 
 def main():
-    basic_cmd = 'CRISPResso -r1 /Users/mckaybowcut/Edilytics/CRISPResso2_tests/cli_integration_tests/inputs/FANC.Cas9.fastq -a CGGATGTTCCAATCAGTACGCAGAGAGTCGCCGTCTCCAAGGTGAAAGCGGAAGTAGGGCCTTCGCGCACCTCATGGAATCCCTTCTGCAGCACCTGGATCGCTTTTCCGAGCTTCTGGCGGTCTCAAGCACTACCTACGTCAGCACCTGGGACCCCGCCACCGTGCGCCGGGCCTTGCAGTGGGCGCGCTACCTGCGCCACATCCATCGGCGCTTTGGTCGG -g GGAATCCCTTCTGCAGCACC --debug 2>&1'
-    subprocess.run(basic_cmd, shell=True, stderr=subprocess.STDOUT, text=True)
+    command = "alias python='python3' && " + sys.argv[1]
+    start = time.time()
+    subprocess.run(command, shell=True, stderr=subprocess.STDOUT, text=True)
+    end = time.time()
+    duration = end - start
+    print(f"Duration: {duration} seconds")
 
 if __name__ == '__main__':
     main()
