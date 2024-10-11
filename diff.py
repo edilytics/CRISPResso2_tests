66973003e7b32f75aaf13d9e693141f39bfaf952
Cole Lyman <cole@colelyman.com>
2024-10-11 14:14:44 -0600

    Have ydiff always output color

--- diff.py
+++ diff.py
@@ -106,7 +106,7 @@ def print_diff(diff_results):
         with tempfile.NamedTemporaryFile(mode='w') as fh:
             fh.writelines(''.join(diff_results))
             fh.flush()
-            subprocess.check_call(f'cat {fh.name} | ydiff -s -w 0 --wrap', shell=True)
+            subprocess.check_call(f'cat {fh.name} | ydiff --color="always" -s -w 0 --wrap', shell=True)
     else:
         for line in diff_results:
             print(line, end='')
